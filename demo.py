7423a7364c41adb0f3a597e0bc703a06830ab950
Mingzhe <57017232+SuperbTUM@users.noreply.github.com>
2021-02-20 17:09:45 +0800

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,24 @@
+from browsermobproxy import Server
+from selenium import webdriver
+import time
+import selenium.webdriver.support.ui as ui
+from selenium.webdriver.chrome.options import Options
+
+
+server = Server(r"browsermob-proxy")
+server.start()
+proxy = server.create_proxy()
+chrome_options = Options()
+chrome_options.add_argument('--proxy-server={0}'.format(proxy.proxy))
+sel = webdriver.Chrome(executable_path=r'chromedriver.exe', chrome_options=chrome_options)
+login_url = ''
+proxy.new_har("w3huawei", options={'captureHeaders': True, 'captureContent': True})
+sel.get(login_url)
+time.sleep(2)
+result = proxy.har
+print(result)
+
+
+server.stop()
+sel.quit()
+
